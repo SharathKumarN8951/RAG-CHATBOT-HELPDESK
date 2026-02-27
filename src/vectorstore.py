a912dfaf316c17660afb677a46b60ca43bd258b7
Sharath Kumar N <sharatnsharu@gmail.com>
2026-02-27 16:23:00 +0530

    Enhance create_vectorstore to handle existing index
    Added functionality to load existing FAISS index or create a new one if it doesn't exist.

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,7 +1,24 @@
+import os
 from langchain_community.vectorstores import FAISS
 
-def create_vectorstore(chunks, embeddings):
-    return FAISS.from_documents(chunks, embeddings)
+
+def create_vectorstore(chunks, embeddings, index_path):
+    # If index already exists → load it
+    if os.path.exists(index_path):
+        print("Loading existing FAISS index...")
+        return FAISS.load_local(
+            index_path,
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
+
+    # Else → create new and save
+    print("Creating new FAISS index...")
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+    vectorstore.save_local(index_path)
+
+    return vectorstore
+
 
 def get_retriever(vectorstore, top_k):
     return vectorstore.as_retriever(search_kwargs={"k": top_k})
